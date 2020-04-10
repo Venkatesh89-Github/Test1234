5045e9bd338db28b52d20fcc78ce30ce25641a3b
Venkatesh89-Github <56060159+Venkatesh89-Github@users.noreply.github.com>
2020-04-10 12:31:15 +0530

    Add files via upload

---
+++ template/Devopstest.ps1
@@ -0,0 +1,12 @@
+﻿
+# Variables for common values
+$resourceGroup = "PowershellRG"
+$location = "EAST US"
+$vmName = "PowershellVM"
+
+# Create a subnet configuration
+$subnetConfig = New-AzVirtualNetworkSubnetConfig -Name PowershellSubnet -AddressPrefix 192.168.1.0/24
+
+# Create a virtual network
+$vnet = New-AzVirtualNetwork -ResourceGroupName $resourceGroup -Location $location `
+  -Name PowershellVNet -AddressPrefix 192.168.0.0/16 -Subnet $subnetConfig
